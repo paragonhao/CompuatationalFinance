808631f8a2299873096712be5cc7b2b2ebba17e9
xiahao <wangxiahaosin@gmail.com>
2019-03-12 20:14:30 -0700

    hw9

--- computationalFinanceProject9/main.cpp
+++ computationalFinanceProject9/main.cpp
@@ -16,13 +16,28 @@ void RunQn1(double WAC, double pv0, double r0, double specialK, double rbar, dou
 
 }
 
-void RunQn2(double WAC, double pv0, double r0, double specialK, double rbar, double sigma, double time, double OAS, bool isOAS){
+double RunQn2(double WAC, double pv0, double r0, double specialK, double rbar, double sigma, double time, double OAS, bool isOAS){
 
-    OAS = -0.0085; // starting position
     double mbsPrice = MortgageBackedSecurities::getNumerixPrepaymentModel(WAC,  pv0,  r0,  specialK,  rbar,  sigma,  time, OAS, isOAS);
     cout << " Price is "<< mbsPrice << endl;
-
+    return OAS;
 }
+
+void RunQn3(double WAC, double pv0, double r0, double specialK, double rbar, double sigma, double time, double OAS,
+            bool isOAS){
+    double p0 = 110000;
+    double y = 0.0005;
+
+    double p_plus = MortgageBackedSecurities::getNumerixPrepaymentModel( WAC,  pv0,  r0,  specialK,  rbar,  sigma,  time, (OAS + y), isOAS);
+    double p_minus = MortgageBackedSecurities::getNumerixPrepaymentModel( WAC,  pv0,  r0,  specialK,  rbar,  sigma,  time, (OAS - y), isOAS);
+
+    double duration = (p_minus - p_plus)/(2 * y * p0);
+    double convexity = (p_plus + p_minus - 2 * p0)/(2 * p0 * y * y);
+
+    cout <<"Duration: " << duration << endl;
+    cout <<"Convexity: " << convexity << endl;
+}
+
 
 int main() {
     double WAC = 0.08;
@@ -37,7 +52,10 @@ int main() {
 
     RunQn1(WAC,  pv0,  r0,  specialK,  rbar,  sigma,  time);
 
+    OAS = -0.0085;
     RunQn2(WAC,  pv0,  r0,  specialK,  rbar,  sigma,  time, OAS, isOAS);
+
+    RunQn3(WAC, pv0, r0, specialK, rbar, sigma, time, OAS, isOAS);
 
 
     return 0;
